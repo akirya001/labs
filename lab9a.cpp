572a2417f33fb268a25b339f3c53d813f00273c3
Kirill Agafonov <akirya01@mail.ru>
2020-06-25 15:20:05 +0400

    second 9a

--- lab9a.cpp
+++ lab9a.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int InputMatr()
+void InputMatr()
 {
     int n;
+    cout<<"Введите размер матрицы: ";
     cin>>n;
-    int a[n][n];
+    int a[n][n],max=0;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
             a[i][j]=rand()%5;
             cout<<a[i][j]<<" ";
+            if(a[i][j] > max)
+            {
+                max = a[i][j];
+            }
         }
-    cout<<endl;
+    cout<<"| max: "<<max<<" "<<endl;
     }      
-    return a[n][n];
 }
+
 
 int main()
 {
+    
     InputMatr();
     return 0;
 }
