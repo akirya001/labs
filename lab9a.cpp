a9e324e8582a3083e740084deb702368cafe69bb
Kirill Agafonov <akirya01@mail.ru>
2020-06-26 11:21:40 +0400

    third 9a

--- lab9a.cpp
+++ lab9a.cpp
@@ -1,31 +1,39 @@
 #include <iostream>
 using namespace std;
 
-void InputMatr()
+int main()
 {
-    int n;
+    int n, *a;
     cout<<"Введите размер матрицы: ";
     cin>>n;
-    int a[n][n],max=0;
+    int array[n],max=0;
+    a = (int*)malloc(n*n * sizeof(int));
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            a[i][j]=rand()%5;
-            cout<<a[i][j]<<" ";
-            if(a[i][j] > max)
+           *(a + i*n + j)=rand()%5;
+            cout<<*(a + i*n + j)<<" ";
+            if(*(a + i*n + j) > max)
             {
-                max = a[i][j];
+                max = *(a + i*n + j);
+                array[i]=j;
             }
         }
-    cout<<"| max: "<<max<<" "<<endl;
-    }      
-}
-
-
-int main()
-{
-    
-    InputMatr();
+    cout<<"| max: "<<max<<" | pos: "<<array[i]<<endl;
+    max=0;
+    }     
+    cout<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (j<array[i])
+                *(a + i*n + j)=0;
+            cout<<*(a + i*n + j)<<" ";
+        }
+    cout<<endl;
+    }  
+    delete a;
     return 0;
 }
